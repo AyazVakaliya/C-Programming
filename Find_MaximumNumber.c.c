c71f5dbe62f855cfa68cf65177ee5bec8f51c9fa
AyazVakaliya <114794576+AyazVakaliya@users.noreply.github.com>
2022-10-13 21:36:17 +0530

    Add files via upload

---
+++ Find_MaximumNumber.c.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the value of firest latter");
+	scanf("%d",&a);
+	printf("enter the value of second latter");
+	scanf("%d",&b);
+	printf("enter the value of third latter");
+	scanf("%d",&c);
+	if(a>b)
+	{
+		if(a>c)
+		printf("a is maximum");
+		
+	if(b>c);	
+	}
+	{
+		if(b>a)
+		printf("b is maximum");
+	}
+	 if (c>a)
+	{
+		if(c>b)
+		printf("c is maximum");
+	}
+	
+	return 0;
+	
+}
