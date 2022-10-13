8736969926f03e2133bd83022192b866f0b4f093
AyazVakaliya <114794576+AyazVakaliya@users.noreply.github.com>
2022-10-13 20:55:19 +0530

    Add files via upload

---
+++ Even-Odd Number.c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("entre the value of firest latter");
+	scanf("%d",&a);
+	if (a%2==0){
+		printf("a is even");
+	}
+	else
+	{
+		printf("a is odd");
+	}
+	return 0;
+}
